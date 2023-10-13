47dfc3b16d8fb99c94229a3b074ab7fc861109c7
Emmanuel <emmanuel.maurice@gmail.com>
2023-10-13 10:37:50 +0200

    dockerfile

--- deploy/api/index.js
+++ deploy/api/index.js
@@ -15,11 +15,6 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to CNAM application." });
-});
-
 const db = require("./models");
 
 db.sequelize.sync()

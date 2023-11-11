8fd0d16cb3047788a678ea21f453d14cc6a67de4
Emmanuel MAURICE <emmanuel.maurice@gmail.com>
2023-11-11 20:34:21 +0100

    controler

--- deploy/api/middleware.php
+++ deploy/api/middleware.php
@@ -2,7 +2,6 @@
 	use Psr\Http\Message\ResponseInterface as Response;
 	use Psr\Http\Message\ServerRequestInterface as Request;
 	use Tuupola\Middleware\HttpBasicAuthentication;
-	use Tuupola\Middleware\HttpBasicAuthentication;
 	use \Firebase\JWT\JWT;
 	
 	const JWT_SECRET = "TP-CNAM";

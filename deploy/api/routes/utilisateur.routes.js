5820aa1870afc91809d3d66144faeb5e5a468bda
Emmanuel MAURICE <emmanuel.maurice@gmail.com>
2023-12-09 18:27:09 +0100

    controler

--- deploy/api/routes/utilisateur.routes.js
+++ deploy/api/routes/utilisateur.routes.js
@@ -5,26 +5,8 @@ module.exports = app => {
   
     var router = require("express").Router();
   
-    // Create a new utilisateur
-    router.post("/", utilisateur.create);
-  
-    // Retrieve all utilisateur
-    router.get("/", checkJwt,utilisateur.findAll);
-
     // login utilisateur
     router.post("/login", utilisateur.login);
-  
-    // Retrieve a single utilisateur with id
-    router.get("/:id",checkJwt, utilisateur.findOne);
-  
-    // Update a utilisateur with id
-    router.put("/:id", checkJwt, utilisateur.update);
-  
-    // Delete a utilisateur with id
-    router.delete("/:id", checkJwt, utilisateur.delete);
-  
-    // Delete all utilisateur
-    router.delete("/", checkJwt,utilisateur.deleteAll);
   
     app.use('/api/utilisateur', router);
   };

8cffc5b692d304fb11a88cabd988c1eb9ce1610d
emmamau <emmanuel.maurice@gmail.com>
2023-12-17 14:51:34 +0100

    Update config.js
    Signed-off-by: emmamau <emmanuel.maurice@gmail.com>

--- deploy/api/config.js
+++ deploy/api/config.js
@@ -3,7 +3,7 @@ module.exports =  {
     BDD : {
     "host" :"dpg-clvf4ida73kc73bp3kfg-a.oregon-postgres.render.com",
     "port" : "",
-    "user" : "cnam_db_akkt_user:",
+    "user" : "cnam_db_akkt_user",
     "password" : "I8TW2jVgiSyQDguxH8De6kWjgJ6VjoR4",
     "bdname" :"cnam_db_akkt" 
     }

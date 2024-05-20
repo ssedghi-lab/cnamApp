32bc7451f55f2bb4c5d17a27c82226cdd3fb1be1
emmamau <emmanuel.maurice@gmail.com>
2024-05-20 10:05:56 +0200

    Update index.js
    Signed-off-by: emmamau <emmanuel.maurice@gmail.com>

--- deploy/api/models/index.js
+++ deploy/api/models/index.js
@@ -18,6 +18,6 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.utilisateur = require("./utilisateur.model.js")(sequelize, Sequelize);
+db.utilisateurs = require("./utilisateurs.model.js")(sequelize, Sequelize);
 
 module.exports = db;

9185ef4ebc1169cb3e5d863385f1f6f7ab602647
Emmanuel MAURICE <emmanuel.maurice@gmail.com>
2024-12-27 09:29:13 +0100

    credentials

--- deploy/api/config.js
+++ deploy/api/config.js
@@ -1,10 +1,12 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-cok0d7gl6cac73e70b2g-a.oregon-postgres.render.com",
+    "host" :"dpg-ctn62ibtq21c73fe3g2g-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "cnam_l5ol_user",
-    "password" : "VoZBXopqdGnJ5Vilgny4Jc0ANIK6XqPY",
-    "bdname" :"cnam_l5ol" 
+    "user" : "cnam_db_v095_user",
+    "password" : "HGxvDFcFWxo8U6RGVMhraHKFoU0ggJKF",
+    "bdname" :"cnam_db_v095" 
     }
 }
+
+
